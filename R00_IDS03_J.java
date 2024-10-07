d84890cff7be465c4bdeae39d48830770350999f
aportillo03 <40402340+aportillo03@users.noreply.github.com>
2024-10-06 20:40:55 -0400

    Update R00_IDS03_J.java (Compliant)

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,7 +1,7 @@
 /*
   Rule 00. Input Validation and Data Sanitization (IDS)
   IDS03-J. Do not log unsanitized user input 
-  Given the non-compliant code below:
+  Given the compliant code below:
 */  
 if (loginSuccessful) {
   logger.severe("User login succeeded for: " + sanitizeUser(username));
