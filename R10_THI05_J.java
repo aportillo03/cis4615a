6d1d8d30ef9b8f3e412149d1ba75634e6d331931
aportillo03 <40402340+aportillo03@users.noreply.github.com>
2024-10-06 20:51:48 -0400

    Update R10_THI05_J.java (Compliant)

--- R10_THI05_J.java
+++ R10_THI05_J.java
@@ -4,24 +4,30 @@ THI05-J. Do not use Thread.stop() to terminate threads
 */
 public final class Container implements Runnable {
   private final Vector<Integer> vector = new Vector<Integer>(1000);
+  private volatile boolean done = false;
  
   public Vector<Integer> getVector() {
     return vector;
   }
  
+  public void shutdown() {
+    done = true;
+  }
+ 
   @Override public synchronized void run() {
     Random number = new Random(123L);
     int i = vector.capacity();
-    while (i > 0) {
+    while (!done && i > 0) {
       vector.add(number.nextInt(100));
       i--;
     }
   }
  
   public static void main(String[] args) throws InterruptedException {
-    Thread thread = new Thread(new Container());
+    Container container = new Container();
+    Thread thread = new Thread(container);
     thread.start();
     Thread.sleep(5000);
-    thread.stop();
+    container.shutdown();
   }
 }
